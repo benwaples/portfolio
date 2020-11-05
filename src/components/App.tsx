9eb9ebe8721cad86fe44bb232c63cf923d7de481
bwapes <benwaples@gmail.com>
2020-11-04 20:51:50 -0800

    bootstrap-react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,15 +5,17 @@ import {
   Route
 } from 'react-router-dom'
 import './App.scss';
-import { About } from './about/About';
-import { Home } from './home/Home'
+// import { About } from './about/About';
+import { Placeholder } from './placeholder/Placeholder'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Home } from './home/Home';
 function App() {
   return (
    <>
    <Router>
      <Switch>
-       <Route exact path="/" component={Home} />
-       <Route exact path="/about" component={About} />
+       <Route exact path="/" component={Placeholder} />
+       <Route exact path="/home" component={Home} />
      </Switch>
    </Router>
    </>
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import './Home.scss'
+import { Container, Jumbotron } from 'react-bootstrap'
 
 export const Home = () => {
   return (
-    <>
-      <div id="home">
-        <h1>I guess this makes me the host that invites guests over before dinner is ready...</h1>
-        <h3> While I have you here, let me introduce myself. I'm Ben Waples, a full-stack software developer. My portfolio is under construction right now, but please find me on <a href="https://github.com/benwaples">Github</a>, <a href="https://www.linkedin.com/in/benwaples/">LinkedIn</a>, or checkout out my <a href="./assets/resume.pdf" target="_blank">resume</a>, to learn more and reach out.</h3>
-        <p>Thanks!</p>
-      <div id="orange-color-block">
-        <img src="./assets/headshot.jpg" alt="ben" />
-      </div>
-      </div>
-      </>
+    <div>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Hi, Im Ben</h1>
+          <p>
+            I am a full-stack software engineer who enjoys finding innovative ways to solve problems, connecting with teammates, and vegetables.
+          </p>
+        </Container>
+      </Jumbotron>
+    </div>
   )
 }
