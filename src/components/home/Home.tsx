d0d9801d31ebd7b6fcf6f320c022994bbac22e05
bwapes <benwaples@gmail.com>
2020-11-14 16:11:59 -0800

    add favicon

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Container, Jumbotron } from 'react-bootstrap'
 import { education, rowing, stAndrews } from '../about/fillers/fillerContent'
 import { PictureLeft } from '../about/fillers/PictureLeft'
 import { PictureRight } from '../about/fillers/PictureRight'
@@ -8,18 +7,21 @@ import './Home.scss'
 export const Home = () => {
   return (
     <>
-    <header id="header">
-      <h1>Ben Waples</h1>
-    </header>
-    <nav>
+      <header id="header">
+        <div id="text">
+          <h1>Ben Waples</h1>
+          <p>Full-Stack Developer</p>
+        </div>
+      </header>
+      <nav>
 
-    </nav>
-    <div id="content">
-    <PictureLeft {...education}/>
-    <PictureRight {...rowing}/>
-    <PictureLeft {...stAndrews}/>
+      </nav>
+      <div id="content">
+        <PictureLeft {...education}/>
+        <PictureRight {...rowing}/>
+        <PictureLeft {...stAndrews}/>
 
-    </div>
+      </div>
     </>
   )
 }
