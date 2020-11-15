dbc6836ea3b07d75a59c0b49109df5dcd92ee386
bwapes <benwaples@gmail.com>
2020-11-14 19:37:56 -0800

    nav bar

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,8 +5,21 @@ import { education, rowing, stAndrews } from '../about/fillers/fillerContent'
 import { PictureLeft } from '../about/fillers/PictureLeft'
 import { PictureRight } from '../about/fillers/PictureRight'
 import './Home.scss'
+// import { Nav } from '../nav/Nav'
 
 export const Home = () => {
+  const aboutRef = React.useRef<HTMLDivElement>(null)
+  const projectsRef = React.useRef<HTMLDivElement>(null)
+  const techRef = React.useRef<HTMLDivElement>(null)
+
+  const handleNav = (reference: React.RefObject<HTMLDivElement>) => {
+    if(reference.current) {
+      reference.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      })
+    }
+  }
   return (
     <>
       <header id="header">
@@ -16,17 +29,20 @@ export const Home = () => {
         </div>
       </header>
       <nav>
-
+        <p onClick={() => handleNav(aboutRef)}>About Me</p>
+        <p onClick={() => handleNav(projectsRef)}>Recent Projects</p>
+        <p onClick={() => handleNav(techRef)}>Tech Stack</p>
+        <p onClick={() => handleNav(techRef)}>Connect</p>
       </nav>
-      <div id="projects">
+      <div ref={projectsRef} id="projects">
         <h1>Projects</h1>
         <Projects />
       </div>
-      <div id="tech">
-        <h1>tech</h1>
+      <div ref={techRef} id="tech">
+        <h1>Tech Stack</h1>
         <Tech />
       </div>
-      <div id="aboutMe">
+      <div ref={aboutRef} id="aboutMe">
         <h1>A little about me</h1>
         <PictureLeft {...education}/>
         <PictureRight {...rowing}/>
