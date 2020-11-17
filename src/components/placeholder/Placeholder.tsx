76cef66b20383e213e09f81a23db18cb71d7587a
bwapes <benwaples@gmail.com>
2020-11-17 09:05:39 -0800

    test photo host

--- src/components/placeholder/Placeholder.tsx
+++ src/components/placeholder/Placeholder.tsx
@@ -10,7 +10,7 @@ export const Placeholder = () => {
         <h3> While I have you here, let me introduce myself. I'm Ben Waples, a full-stack software developer. My portfolio is under construction right now, but please find me on <a href="https://github.com/benwaples">Github</a>, <a href="https://www.linkedin.com/in/benwaples/">LinkedIn</a>, or checkout out my <a href="./assets/resume.pdf" target="_blank">resume</a>, to learn more and reach out.</h3>
         <p>Thanks!</p>
       <div id="orange-color-block">
-      <Link to="/home"><img src="./assets/headshot.jpg" alt="ben" /></Link>
+        <Link to="/home"><img src="./assets/headshot.jpg" alt="ben" /></Link>
       </div>
       </div>
       </>
