d66cb1e869c378bfcc1098bfd7ae1e33401afc0b
bwapes <benwaples@gmail.com>
2020-11-17 08:54:19 -0800

    add secret button

--- src/components/placeholder/Placeholder.tsx
+++ src/components/placeholder/Placeholder.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Placeholder.scss'
 
 export const Placeholder = () => {
   return (
     <>
       <div id="home">
-        <h1>I guess this makes me the host that invites guests over before dinner is ready...</h1>
+        <h1>I guess this makes me the host that invites guests over before dinner is <Link to="/home">ready...</Link></h1>
         <h3> While I have you here, let me introduce myself. I'm Ben Waples, a full-stack software developer. My portfolio is under construction right now, but please find me on <a href="https://github.com/benwaples">Github</a>, <a href="https://www.linkedin.com/in/benwaples/">LinkedIn</a>, or checkout out my <a href="./assets/resume.pdf" target="_blank">resume</a>, to learn more and reach out.</h3>
         <p>Thanks!</p>
       <div id="orange-color-block">
