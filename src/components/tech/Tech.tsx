70b45935170d4dd1706f0ad09c1fb35f56f718c4
bwapes <benwaples@gmail.com>
2020-11-18 16:31:21 -0800

    tech stack logos

--- src/components/tech/Tech.tsx
+++ src/components/tech/Tech.tsx
@@ -5,20 +5,38 @@ export const Tech = () => {
   return (
     <>
       <div id="techStack">
-        <img src="./assets/React.png" title="React" alt="React" />
-        <img src="./assets/Redux.png" alt="Redux" />
-        <img src="./assets/html.png" alt="HTML" />
+        <section>
+          <h3>Development</h3>
+          <img src="./assets/js.png" alt="JavaScript" />
+          <img src="./assets/ts.png" alt="TypeScript" />
+          <img src="./assets/html.png" alt="HTML" />
         <img src="./assets/css.png" alt="CSS" />
-        <img src="./assets/scss.png" alt="SCSS" />
-        <img src="./assets/js.png" alt="JavaScript" />
-        <img src="./assets/ts.png" alt="TypeScript" />
-        <img src="./assets/node.png" alt="Node" />
+        </section>
+        <section>
+          <h3>Libraries and Frameworks</h3>
+          <img src="./assets/React.png" title="React" alt="React" />
+          <img src="./assets/Redux.png" alt="Redux" />
+          <img src="./assets/scss.png" alt="SCSS" />
+          <img src="./assets/node.png" alt="Node" />
         <img src="./assets/Expressjs.png" alt="Express JS" />
         <img src="./assets/pug.png" alt="PugJs" />
-        <img src="./assets/jest.png" alt="Jest Testing" />
+        </section>
+        <section>
+          <h3>Dev Tools</h3>
+          <img src="./assets/githubLarge.png" alt="Github" />
+          <img src="./assets/heroku.png" alt="Heroku" />
+          <img src="./assets/netlify.png" alt="Netlify" />
+        </section>
+        <section>
+          <h3>Testing</h3>
+          <img src="./assets/jest.png" alt="Jest Testing" />
         <img src="./assets/reacttestinglibrary.png" alt="React Testing Library" />
         <img src="./assets/qunit.png" alt="Qunit Test" />
-        <img src="./assets/postgresql.png" alt="postgreSQL" />
+        </section>
+        <section>
+          <h3>Database</h3>
+          <img src="./assets/postgresql.png" alt="postgreSQL" />
+        </section>
       </div>
       <div className="horizontalLine"></div>
     </>
