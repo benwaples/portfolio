031139ce3096f5d01e4466f5dad8bf49c1329a36
bwapes <benwaples@gmail.com>
2020-12-18 14:02:16 -0800

    Airbnb prettier config

--- src/components/about/fillers/fillerContent.ts
+++ src/components/about/fillers/fillerContent.ts
@@ -1,22 +1,23 @@
-import { FillerType } from "../../../types";
+import { FillerType } from '../../../types';
 
 export const education: FillerType = {
   title: 'Western Washington University',
   imageSrc: './assets/western.jpeg',
   caption: 'Old Main',
-  description: `My coach would say 'Rowing is not a fun sport, its hardly a sport', and I would have to agree with that. However, I wouldn't trade a minute on the water! Everyday I was reminded of lessons in patience, working together, communication, and so much more.`
-}
+  description: `My coach would say 'Rowing is not a fun sport, its hardly a sport', and I would have to agree with that. However, I wouldn't trade a minute on the water! Everyday I was reminded of lessons in patience, working together, communication, and so much more.`,
+};
 
 export const stAndrews: FillerType = {
   title: 'St. Andrews University - Scotland',
   imageSrc: './assets/stAndrews.jpeg',
   caption: 'The Quad at St. Andrews in Saint Andrews, Scotland!',
-  description: `Studying at one of the U.Ks top university's and immersing myself in the deep scottish traditions.`
-}
+  description: `Studying at one of the U.Ks top university's and immersing myself in the deep scottish traditions.`,
+};
 
 export const rowing: FillerType = {
   title: 'Rowing at Western Washington University',
   imageSrc: './assets/rowing.jpeg',
-  caption: 'My teammates and I waiting for the next drill on Lake Whatcom in Bellingham, WA.',
-  description: `My coach would say 'Rowing is not a fun sport, its hardly a sport', and I would have to agree with that. However, I wouldn't trade a minute on the water! Everyday I was reminded of lessons in patience, working together, communication, and so much more.`
-}
+  caption:
+    'My teammates and I waiting for the next drill on Lake Whatcom in Bellingham, WA.',
+  description: `My coach would say 'Rowing is not a fun sport, its hardly a sport', and I would have to agree with that. However, I wouldn't trade a minute on the water! Everyday I was reminded of lessons in patience, working together, communication, and so much more.`,
+};
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,43 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import { Tech } from '../tech/Tech'
-import { Projects } from '../projects/Projects'
-import './Home.scss'
-import { About } from '../about/About'
-import { Connect } from '../connect/Connect'
-import { isMobile } from 'react-device-detect'
+import React, { useEffect, useState } from 'react';
+import { isMobile } from 'react-device-detect';
+import { Tech } from '../tech/Tech';
+import { Projects } from '../projects/Projects';
+import './Home.scss';
+import About from '../about/About';
+import { Connect } from '../connect/Connect';
 
-export const Home = () => {
-  const [source, setSource] = useState('https://media.giphy.com/media/LLd6Ma5vQtXyw/giphy.gif')
-  const aboutRef = React.useRef<HTMLDivElement>(null)
-  const projectsRef = React.useRef<HTMLDivElement>(null)
-  const techRef = React.useRef<HTMLDivElement>(null)
-  const connect = React.useRef<HTMLDivElement>(null)
+export default Home = (): JSX.Element => {
+  const [source, setSource] = useState(
+    'https://media.giphy.com/media/LLd6Ma5vQtXyw/giphy.gif'
+  );
+  const aboutRef = React.useRef<HTMLDivElement>(null);
+  const projectsRef = React.useRef<HTMLDivElement>(null);
+  const techRef = React.useRef<HTMLDivElement>(null);
+  const connect = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const img = new Image();
-    img.src = './assets/pacificCity.jpg'
-    img.onload = () => setSource('./assets/pacificCity.jpg')
-  }, [])
-  
+    img.src = './assets/pacificCity.jpg';
+    img.onload = () => setSource('./assets/pacificCity.jpg');
+  }, []);
+
   const handleNav = (reference: React.RefObject<HTMLDivElement>) => {
-    if(reference.current) {
+    if (reference.current) {
       reference.current.scrollIntoView({
         behavior: 'smooth',
         block: 'start',
-      })
+      });
     }
-  }
+  };
 
-  if(isMobile) alert('please view this site on a desktop for best experience, sorry for the inconvenience')
+  if (isMobile)
+    alert(
+      'please view this site on a desktop for best experience, sorry for the inconvenience'
+    );
 
   return (
     <>
-      <header className={source.includes('giphy') ? 'loading' : ''} style={{backgroundImage: `url(${source})`}} id="header">
+      <header
+        className={source.includes('giphy') ? 'loading' : ''}
+        style={{ backgroundImage: `url(${source})` }}
+        id="header"
+      >
         <div id="text">
           <h1>Ben Waples</h1>
           <p>Full-Stack Developer</p>
         </div>
-        <img onClick={() => handleNav(projectsRef)} src="./assets/scroll-down.png" className="fadeIn bounce-2" alt="scroll down"/>
+        <button onClick={() => handleNav(projectsRef)} type="button">
+          <img
+            src="./assets/scroll-down.png"
+            className="fadeIn bounce-2"
+            alt="scroll down"
+          />
+        </button>
       </header>
       <nav>
         <p onClick={() => handleNav(projectsRef)}>Recent Projects</p>
@@ -56,12 +71,12 @@ export const Home = () => {
       <div ref={aboutRef} id="aboutMe">
         <h1>A little about me</h1>
         <About />
-      <div className="horizontalLine"></div>
+        <div className="horizontalLine" />
       </div>
       <div ref={connect} id="connect">
         <h1>Lets Connect!</h1>
         <Connect />
       </div>
     </>
-  )
-}
+  );
+};
