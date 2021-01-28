ae17d0f1ec28edff437ecb6f78de13cb13abdf4d
bwapes <benwaples@gmail.com>
2021-01-27 16:50:22 -0800

    fix about me spelling

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -12,8 +12,7 @@ export default function About(): JSX.Element {
           <p>
             Hi! I&#39;m Ben, a full-stack software developer from Portland, OR.
             My background in marketing and sales has driven my passion in
-            building online tools to help better serve clients. Coding is 
-            my tool choice to do thi! I thrive in team environments and on solo
+            building online tools to help better serve clients. I thrive in team environments and on solo
             projects. I am committed to continuously learning new technologies and work
             flows in order to build cutting edge products. See the photos below
             to learn more about me!
