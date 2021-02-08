b7760703f5912d92722f4351deaab24262465937
bwapes <benwaples@gmail.com>
2021-02-08 09:33:55 -0800

    connect styling

--- src/components/pageNotFound/PageNotFound.tsx
+++ src/components/pageNotFound/PageNotFound.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './pageNotFound.scss';
 
 export default function PageNotFound() {
   return (
-    <div>
+    <div id="page-not-found">
       <h1>404</h1>
+      <h3>This isn&#39;t the page you&#39;re looking for</h3>
     </div>
   );
 }
