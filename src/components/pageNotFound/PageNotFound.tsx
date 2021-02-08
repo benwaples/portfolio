e37d02be65e03664b452b65a68c6b63684c5b135
bwapes <benwaples@gmail.com>
2021-02-08 11:04:24 -0800

    coolCircle components

--- src/components/pageNotFound/PageNotFound.tsx
+++ src/components/pageNotFound/PageNotFound.tsx
@@ -1,15 +1,32 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import CoolCircle from './coolCircle/CoolCircle';
+import { CoolCircleSmall, CoolCircleLarge } from './coolCircle/CoolCircle';
 import './pageNotFound.scss';
 
 export default function PageNotFound(): JSX.Element {
+  const smallGreenCircle = {
+    top: '20%',
+    left: '-1%',
+    lineColorThin: 'white',
+    lineColorThick: 'green',
+  };
+
+  const largeBlueCircle = {
+    top: '10%',
+    left: '90%',
+    lineColorThin: 'white',
+    // lineColorThick: 'green',
+    rotate: 80,
+  };
+
   return (
     <div id="page-not-found">
       <h1>Page Not Found</h1>
       <h3>
         Head back <a href="./">home</a>
       </h3>
-      <CoolCircle />
+      <CoolCircleSmall {...smallGreenCircle} />
+      <CoolCircleLarge {...largeBlueCircle} />
     </div>
   );
 }
--- src/components/pageNotFound/coolCircle/CoolCircle.tsx
+++ src/components/pageNotFound/coolCircle/CoolCircle.tsx
@@ -1,12 +1,63 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import './coolCircle.scss';
 
-export default function CoolCircle(): JSX.Element {
+interface CircleProps {
+  lineColorThick?: string;
+  lineColorThin?: string;
+  top: string;
+  left: string;
+  size?: string;
+  rotate?: number;
+}
+
+function RotatedLine({
+  lineColorThick,
+  rotate,
+}: Pick<CircleProps, 'lineColorThick' | 'rotate'>) {
   return (
-    <div id="circle-container">
-      <div id="top" className="rotated-line" />
-      <div id="middle" className="rotated-line" />
-      <div id="bottom" className="rotated-line" />
+    <div
+      className="rotated-line"
+      style={{
+        background: lineColorThick || '#33658a',
+        transform: `rotate(${rotate}deg)`,
+      }}
+    />
+  );
+}
+
+export function CoolCircleSmall({
+  lineColorThick,
+  lineColorThin,
+  top,
+  left,
+  size = '50px',
+  rotate = 160,
+}: Partial<CircleProps>): JSX.Element {
+  return (
+    <div
+      id="circle-container"
+      style={{
+        background: lineColorThin,
+        position: 'absolute',
+        top,
+        left,
+        width: size || '50px',
+        height: size || '50px',
+      }}
+    >
+      <RotatedLine {...{ lineColorThick, rotate }} />
+      <RotatedLine {...{ lineColorThick, rotate }} />
+      <RotatedLine {...{ lineColorThick, rotate }} />
     </div>
   );
+}
+
+export function CoolCircleMedium(props: CircleProps): JSX.Element {
+  return <CoolCircleSmall {...{ ...props, size: '100px' }} />;
+}
+
+export function CoolCircleLarge(props: CircleProps): JSX.Element {
+  return <CoolCircleSmall {...{ ...props, size: '200px' }} />;
 }
