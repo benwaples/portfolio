a7fd5837088d0a74cf1d54a532c35e965a7642a4
benwaples <benwaples@gmail.com>
2021-03-22 21:58:37 -0700

    remove descriptions

--- src/components/about/assets.ts
+++ src/components/about/assets.ts
@@ -4,7 +4,7 @@ export const imageArray = [
     original: './assets/western.jpeg',
     // sizes: '50%',
     originalClass: 'carousel-image',
-    description:
+    description1:
       'I studied Business Administration with a concentration in Marketing and a minor in Economics. I also rowed all 4 years I was at WWU!',
     originalTitle: 'Western Washington University',
   },
@@ -12,7 +12,7 @@ export const imageArray = [
     original: './assets/stAndrews.jpeg',
     // sizes: '50%',
     originalClass: 'carousel-image',
-    description:
+    description1:
       'The Quad at St.Andrews where I studied Corporate Social Responsibility and Finance for 4 months',
     originalTitle: 'St. Andrews University - Scotland',
   },
@@ -20,7 +20,7 @@ export const imageArray = [
     original: './assets/rowing.jpeg',
     // sizes: '50%',
     originalClass: 'carousel-image',
-    description:
+    description1:
       'My coach would say "Rowing is not a fun sport, its hardly a sport", and I would have to agree with that.',
     originalTitle:
       'My teammates and I waiting for the next drill on Lake Whatcom in Bellingham, WA.',
@@ -29,7 +29,7 @@ export const imageArray = [
     original: './assets/denver_food.jpg',
     // sizes: '500px',
     originalClass: 'carousel-image',
-    description:
+    description1:
       'I love finding new places too eat and random items on the menu... especially if it includes tatter tots :) ',
     originalTitle: 'Street tacos and loaded tots from Denver!',
   },
@@ -37,7 +37,7 @@ export const imageArray = [
     original: './assets/plums.jpg',
     // sizes: '50%',
     originalClass: 'carousel-image',
-    description:
+    description1:
       'This picture exists because I am a hobbyist photographer and I really love fresh plums.',
     originalTitle: 'Plums and apples fresh off the tree',
   },
