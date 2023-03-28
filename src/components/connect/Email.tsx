398d1824c0b66fc52cf92e36ee1e3bea281c8912
benwaples <benwaples@gmail.com>
2023-03-28 16:22:40 -0700

    connect layout

--- src/components/connect/Email.tsx
+++ src/components/connect/Email.tsx
@@ -9,33 +9,27 @@ export default function Email(): JSX.Element {
     );
   }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input id="name" type="name" name="name" placeholder="name" />
-        <ValidationError
-          prefix="name"
-          field="name"
-          errors={state.errors}
-          required
-        />
-        <input
-          id="email"
-          type="email"
-          name="email"
-          placeholder="email"
-          required
-        />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
-        <textarea placeholder="message" id="message" name="message" />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
-        />
-        <button type="submit" disabled={state.submitting}>
-          Submit
-        </button>
-      </form>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input id="name" type="name" name="name" placeholder="name" />
+      <ValidationError
+        prefix="name"
+        field="name"
+        errors={state.errors}
+        required
+      />
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="email"
+        required
+      />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
+      <textarea placeholder="message" id="message" name="message" />
+      <ValidationError prefix="Message" field="message" errors={state.errors} />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
   );
 }
